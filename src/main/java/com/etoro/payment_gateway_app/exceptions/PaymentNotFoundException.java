d6a6c57a5445018a8206b7f276d124eeca6540ef
Etoro Umoren <torohkiss@yahoo.com>
2026-07-26 19:35:14 +0100

    fix: PaymentService(add capture, void and refund methods.) fix: PaymentNotFoundException

--- src/main/java/com/etoro/payment_gateway_app/exceptions/PaymentNotFoundException.java
+++ src/main/java/com/etoro/payment_gateway_app/exceptions/PaymentNotFoundException.java
@@ -2,9 +2,11 @@ package com.etoro.payment_gateway_app.exceptions;
 
 import org.springframework.http.HttpStatus;
 
+import java.util.UUID;
+
 public class PaymentNotFoundException extends PaymentGatewayException{
 
-    public PaymentNotFoundException(String message) {
-        super(message, HttpStatus.NOT_FOUND);
+    public PaymentNotFoundException(UUID paymentReference) {
+        super("Payment not found with reference: " + paymentReference, HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/etoro/payment_gateway_app/service/PaymentService.java
+++ src/main/java/com/etoro/payment_gateway_app/service/PaymentService.java
@@ -2,10 +2,9 @@ package com.etoro.payment_gateway_app.service;
 
 
 import com.etoro.payment_gateway_app.client.BankClient;
-import com.etoro.payment_gateway_app.dto.AuthorizationResponse;
-import com.etoro.payment_gateway_app.dto.AuthorizePaymentRequest;
-import com.etoro.payment_gateway_app.dto.AuthorizeRequest;
-import com.etoro.payment_gateway_app.dto.PaymentResponse;
+import com.etoro.payment_gateway_app.dto.*;
+import com.etoro.payment_gateway_app.exceptions.InvalidStateTransitionException;
+import com.etoro.payment_gateway_app.exceptions.PaymentNotFoundException;
 import com.etoro.payment_gateway_app.model.IdempotencyKey;
 import com.etoro.payment_gateway_app.model.Payment;
 import com.etoro.payment_gateway_app.model.PaymentStatus;
@@ -18,6 +17,7 @@ import tools.jackson.databind.ObjectMapper;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PaymentService {
@@ -41,6 +41,8 @@ public class PaymentService {
 
     @Transactional
     public PaymentResponse authorize(AuthorizePaymentRequest request, String idempotencyKey) {
+
+        // Check idempotency key
         Optional<IdempotencyKey> existingKey = idempotencyRepository.findByIdempotencyKey(idempotencyKey);
 
         if(existingKey.isPresent()){
@@ -53,12 +55,13 @@ public class PaymentService {
                         PaymentResponse.class
                 );
             } catch (JacksonException e) {
-                throw new IllegalStateException("Couldnt process checks", e);
+                throw new IllegalStateException("Couldn't process checks", e);
             }
 
             return response;
         }
 
+        // Create payment, if key doesn't exist
         Payment payment = new Payment();
         payment.setOrderId(request.getOrderId());
         payment.setCustomerId(request.getCustomerId());
@@ -69,8 +72,11 @@ public class PaymentService {
         payment.setCardCvv(request.getCardDetails().getCvv());
         payment.setStatus(PaymentStatus.PENDING);
 
+
+        // Save the payment
         paymentRepository.save(payment);
 
+        // Calling the bank, using AuthorizeRequest DTO
         AuthorizeRequest authorizeRequest = new AuthorizeRequest(
                 request.getAmount(),
                 request.getCardDetails()
@@ -84,6 +90,7 @@ public class PaymentService {
 
         IdempotencyKey key = new IdempotencyKey();
 
+        // Getting paymentResponse
         PaymentResponse paymentResponse = new PaymentResponse(
                 payment.getId(),
                 payment.getStatus(),
@@ -103,6 +110,203 @@ public class PaymentService {
             throw new IllegalStateException("Failed to serialize payment response", e);
         }
 
+        idempotencyRepository.save(key);
+
+        return paymentResponse;
+    }
+
+
+    @Transactional
+    public PaymentResponse capture(UUID paymentReference, String idempotencyKey) {
+
+        // Check idempotency key
+        Optional<IdempotencyKey> existingKey = idempotencyRepository.findByIdempotencyKey(idempotencyKey);
+
+        if(existingKey.isPresent()) {
+
+            PaymentResponse response;
+
+            try {
+                response = objectMapper.readValue(
+                        existingKey.get().getResponseBody(),
+                        PaymentResponse.class
+                );
+            } catch (JacksonException e) {
+                throw new IllegalStateException("Couldn't process response", e);
+            }
+
+            return response;
+        }
+
+        // Find the payment
+        Payment payment = paymentRepository.findById(paymentReference)
+                .orElseThrow(() -> new PaymentNotFoundException(paymentReference));
+
+
+        // Validation for capturing
+        if(!payment.canTransitionTo(PaymentStatus.CAPTURED)) {
+            throw new InvalidStateTransitionException(
+                    "Cannot capture payment in state " + payment.getStatus()
+            );
+        }
+
+        // Call the Bank
+        CaptureResponse bankResponse = bankClient.capture(payment.getBankAuthId(), idempotencyKey);
+
+        payment.setStatus(PaymentStatus.CAPTURED);
+        payment.setCapturedAt(LocalDateTime.now());
+        payment.setBankCaptureId(bankResponse.getCaptureId());
+
+        // Create paymentResponse
+        PaymentResponse paymentResponse =
+                new PaymentResponse(
+                        payment.getId(),
+                        payment.getStatus(),
+                        payment.getAmount(),
+                        payment.getOrderId(),
+                        payment.getCustomerId()
+                );
+
+        // Save Idempotency record
+        IdempotencyKey key = new IdempotencyKey();
+        key.setIdempotencyKey(idempotencyKey);
+        key.setPaymentId(payment.getId());
+        key.setOperation("CAPTURE");
+
+        try {
+            String responseBody = objectMapper.writeValueAsString(paymentResponse);
+            key.setResponseBody(responseBody);
+        } catch (JacksonException e) {
+            throw new IllegalStateException("Couldn't get response", e);
+        }
+        idempotencyRepository.save(key);
+
+        return paymentResponse;
+
+    }
+
+
+    @Transactional
+    public PaymentResponse voidAuthorization(UUID paymentReference, String idempotencyKey) {
+
+        // Check for idempotencyKey
+        Optional<IdempotencyKey> existingKey = idempotencyRepository.findByIdempotencyKey(idempotencyKey);
+
+        if(existingKey.isPresent()) {
+            PaymentResponse response;
+
+            try {
+                response = objectMapper.readValue(
+                        existingKey.get().getResponseBody(),
+                        PaymentResponse.class
+                );
+            } catch (JacksonException e) {
+                throw new IllegalStateException("Couldn't get response", e);
+            }
+
+            return response;
+        }
+
+        Payment payment = paymentRepository.findById(paymentReference)
+                .orElseThrow(() -> new PaymentNotFoundException(paymentReference));
+
+        // Validation for voiding
+        if(!payment.canTransitionTo(PaymentStatus.VOIDED)) {
+            throw new InvalidStateTransitionException(
+                    "Cannot void payment in state " + payment.getStatus()
+            );
+        }
+
+        VoidResponse bankResponse = bankClient.voidAuthorization(payment.getBankAuthId(), idempotencyKey);
+
+        payment.setStatus(PaymentStatus.VOIDED);
+        payment.setVoidedAt(LocalDateTime.now());
+        payment.setBankVoidId(bankResponse.getVoidId());
+
+        // Create paymentResponse
+        PaymentResponse paymentResponse =
+                new PaymentResponse(
+                        payment.getId(),
+                        payment.getStatus(),
+                        payment.getAmount(),
+                        payment.getOrderId(),
+                        payment.getCustomerId()
+                );
+
+        IdempotencyKey key = new IdempotencyKey();
+        key.setIdempotencyKey(idempotencyKey);
+        key.setPaymentId(payment.getId());
+        key.setOperation("VOID");
+
+        try {
+            String responseBody = objectMapper.writeValueAsString(paymentResponse);
+            key.setResponseBody(responseBody);
+        } catch (JacksonException e) {
+            throw new IllegalStateException("Couldn't get response", e);
+        }
+        idempotencyRepository.save(key);
+
+        return paymentResponse;
+    }
+
+    @Transactional
+    public PaymentResponse refund(UUID paymentReference, String idempotencyKey) {
+
+        // Check for idempotencyKey
+        Optional<IdempotencyKey> existingKey = idempotencyRepository.findByIdempotencyKey(idempotencyKey);
+
+        if(existingKey.isPresent()) {
+            PaymentResponse response;
+
+            try {
+                response = objectMapper.readValue(
+                        existingKey.get().getResponseBody(),
+                        PaymentResponse.class
+                );
+            } catch (JacksonException e) {
+                throw new IllegalStateException("Couldn't get response", e);
+            }
+
+            return response;
+        }
+
+        Payment payment = paymentRepository.findById(paymentReference)
+                .orElseThrow(() -> new PaymentNotFoundException(paymentReference));
+
+        // Validation for refund
+        if(!payment.canTransitionTo(PaymentStatus.REFUNDED)) {
+            throw new InvalidStateTransitionException(
+                    "Cannot refund payment in state " + payment.getStatus()
+            );
+        }
+
+        RefundResponse bankResponse = bankClient.refund(payment.getBankCaptureId(), idempotencyKey);
+
+        payment.setStatus(PaymentStatus.REFUNDED);
+        payment.setRefundedAt(LocalDateTime.now());
+        payment.setBankRefundId(bankResponse.getRefundId());
+
+        // Create paymentResponse
+        PaymentResponse paymentResponse =
+                new PaymentResponse(
+                        payment.getId(),
+                        payment.getStatus(),
+                        payment.getAmount(),
+                        payment.getOrderId(),
+                        payment.getCustomerId()
+                );
+
+        IdempotencyKey key = new IdempotencyKey();
+        key.setIdempotencyKey(idempotencyKey);
+        key.setPaymentId(payment.getId());
+        key.setOperation("REFUND");
+
+        try {
+            String responseBody = objectMapper.writeValueAsString(paymentResponse);
+            key.setResponseBody(responseBody);
+        } catch (JacksonException e) {
+            throw new IllegalStateException("Couldn't get response", e);
+        }
         idempotencyRepository.save(key);
 
         return paymentResponse;
