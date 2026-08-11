f745164e8a2bc2a5c460b4d4e37efc8dd5a888ff
Etoro Umoren <torohkiss@yahoo.com>
2026-08-11 16:13:05 +0100

    fix: PaymentController(update api paths)

--- src/main/java/com/etoro/payment_gateway_app/controller/PaymentController.java
+++ src/main/java/com/etoro/payment_gateway_app/controller/PaymentController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/payments")
+@RequestMapping("/api/v1")
 public class PaymentController {
 
     private final PaymentService paymentService;
